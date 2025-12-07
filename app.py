8c6ce8775ebcde111e98de0df10f98c940422b5d
noine <6740151822@student.chula.ac.th>
2025-12-08 00:16:03 +0700

    um

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import google.generativeai as genai
-import os
 import json
 import matplotlib.pyplot as plt 
 import plotly.express as px
