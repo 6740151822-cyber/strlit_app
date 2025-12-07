2d1dbdefddd9be517dcfca38f4ecbe4ec077c8dc
noine <6740151822@student.chula.ac.th>
2025-12-07 12:40:25 +0700

    hehe

--- app.py
+++ app.py
@@ -1,1 +1,18 @@
-#skibidi
+import streamlit as st
+import pandas as pd
+import numpy as np
+import google.generativeai as genai
+import os
+import json
+
+st.title("Simple Data Analysis App")
+
+api_key = "AIzaSyBEwVeQhjuPvUKBF2GprB3xCosVzmspgxY"
+
+genai.configure(api_key=api_key)
+model = genai.GenerativeModel("gemini-2.5-flash-lite")
+
+prompt = "Explain the difference between a Generative model and a Discriminative model in simple terms."
+
+response = model.generate_content(prompt)
+print(response.text)
